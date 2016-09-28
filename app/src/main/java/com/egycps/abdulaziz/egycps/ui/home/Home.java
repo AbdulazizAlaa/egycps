089320aa82408c01d76efdea8ae2bae98ec985f0
Abdulaziz Alaa <abdulazizalaa@ymail.com>
2016-09-28 11:17:38 +0200

    fixed home notification layout to be more responsive

--- app/src/main/java/com/egycps/abdulaziz/egycps/ui/home/Home.java
+++ app/src/main/java/com/egycps/abdulaziz/egycps/ui/home/Home.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.egycps.abdulaziz.egycps.R;
 import com.egycps.abdulaziz.egycps.utils.GlobalEntities;
@@ -13,17 +15,23 @@ public class Home extends Activity implements View.OnClickListener{
 
 
     LinearLayout newsLL, offersLL, libraryLL, magazineLL, membersLL, contactsLL;
+    FrameLayout newsNotificationBadge, offersNotificationBadge;
+
+    TextView newsNotificationText, offersNotificationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Log.i(GlobalEntities.HOME_ACTIVITY_TAG, "onCreate: home");
+
         init();
+
     }
 
 
     private void init(){
+        //initializing menu items
         newsLL = (LinearLayout) findViewById(R.id.home_menu_news_item);
         offersLL = (LinearLayout) findViewById(R.id.home_menu_offers_item);
         libraryLL = (LinearLayout) findViewById(R.id.home_menu_library_item);
@@ -31,6 +39,15 @@ public class Home extends Activity implements View.OnClickListener{
         membersLL = (LinearLayout) findViewById(R.id.home_menu_members_item);
         contactsLL = (LinearLayout) findViewById(R.id.home_menu_contacts_item);
 
+        //initializing notification badges
+        newsNotificationBadge = (FrameLayout) findViewById(R.id.home_menu_news_notification_badge);
+        offersNotificationBadge = (FrameLayout) findViewById(R.id.home_menu_offers_notification_badge);
+
+        //initializing notification text
+        newsNotificationText = (TextView) findViewById(R.id.home_menu_news_notification_text);
+        offersNotificationText = (TextView) findViewById(R.id.home_menu_offers_notification_text);
+
+        //adding on click listeners
         newsLL.setOnClickListener(this);
         offersLL.setOnClickListener(this);
         libraryLL.setOnClickListener(this);
